60a4838c82766ca3b6632c0946565b82a1bba31a
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-08 16:54:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,2 +1,13 @@
-# Your code goes here!
-yield
+class Anagram
+attr_accessor :word
+  
+  def initialize (word)
+  @word = word
+  
+  end
+  def match(word)
+    word.each do |letter|
+      if letter.detect
+    end
+  end
+end
